b25fa53ca8419acbaeb3332f3d8515c5ba732f23
Mark Bell <me@markb.co.uk>
2015-04-08 08:52:44 +0100

    Disable HandleErrorAttribute filter

--- Vault/App_Start/FilterConfig.cs
+++ Vault/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Vault
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            // filters.Add(new HandleErrorAttribute());
         }
     }
 }
