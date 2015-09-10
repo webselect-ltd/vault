b0766488263cbb8ea5b4095e6e7f78f6b52ff7f8
Mark Bell <me@markb.co.uk>
2015-09-10 09:10:33 +0100

    Remove unused using statements

--- Vault/Models/SetDevCookieViewModel.cs
+++ Vault/Models/SetDevCookieViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Vault.Models
+﻿namespace Vault.Models
 {
     public class SetDevCookieViewModel
     {
