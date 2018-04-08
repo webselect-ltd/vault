484b7a9eb499e53ca009e6ad001b582aea85a196
Mark Bell <me@markb.co.uk>
2018-04-08 09:25:39 +0100

    Exclude node_modules is ts-loader config

--- Vault/webpack.config.common.js
+++ Vault/webpack.config.common.js
@@ -32,7 +32,7 @@ module.exports = {
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
