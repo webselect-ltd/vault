99191162c70dc45f6f6a133f73af81c67f787558
Mark Bell <me@markb.co.uk>
2018-04-08 16:56:08 +0100

    Webpack compilation is now handled by Gulp tasks

---
+++ Vault/gulpfile.js
@@ -0,0 +1,12 @@
+﻿/// <binding ProjectOpened='watch-app, watch-tests' />
+
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+
+const webpackCmd = `webpack${process.platform === 'win32' ? '.cmd' : ''}`;
+
+const buildAppArgs = ['--watch', '--config=webpack.config.app', '--mode=development'];
+const buildTestsArgs = ['--watch', '--config=webpack.config.tests', '--mode=development'];
+
+gulp.task('watch-app', done => spawn(webpackCmd, buildAppArgs, { stdio: 'inherit' }));
+gulp.task('watch-tests', done => spawn(webpackCmd, buildTestsArgs, { stdio: 'inherit' }));
