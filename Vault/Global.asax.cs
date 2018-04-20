f3f9eb366d23253f3b844f929e0cf3c55a61b5d3
Mark Bell <me@markb.co.uk>
2018-04-20 09:59:33 +0100

    Formatting and cleanup

--- Vault/Global.asax.cs
+++ Vault/Global.asax.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Microsoft.Practices.Unity;
-using Vault.Models;
 using Vault.Support;
 
 namespace Vault
