webselect-ltd/vault

commit b3f0185a62fce57a3de82bb8d1cd7f67b5cb6737

Final refactoring push
The main change is that multiple record updates and password changes are now done within SQL transactions. This avoids the mess you could previously have gotten into where half a user's records are encrypted using one password and half with the other...

Other changes:

- Split controller out into credential operations and other concerns
- Create and Update are now separate operations and endpoints
- Move SQL strings into static class
- Reordering, renaming and tidying

Vault/Content/Js/main.ts
@@ -236,9 +236,10 @@ function hideModal(e: JQuery.Event) {
 
 async function editCredential(credentialId: string) {
     const credential = await repository.loadCredential(credentialId);
+    const templateModel = Object.assign({ new: false }, credential);
     showModal({
         title: 'Edit Credential',
-        content: templates.credentialForm(credential),
+        content: templates.credentialForm(templateModel),
         showAccept: true,
         acceptText: 'Save',
         onaccept: (): void => {
@@ -405,7 +406,7 @@ ui.newButton.on('click', e => {
     e.preventDefault();
     showModal({
         title: 'Add Credential',
-        content: templates.credentialForm({}),
+        content: templates.credentialForm({ new: true }),
         showAccept: true,
         acceptText: 'Save',
         onaccept: (): void => {
@@ -461,6 +462,8 @@ ui.body.on('submit', '#credential-form', async e => {
     $('#validation-message').remove();
     form.find('div.has-error').removeClass('has-error');
 
+    const isNew = form.find('[name=new]').val() === 'true';
+
     const credential = getCredentialFromUI(form);
 
     const errors = validateCredential(credential);
@@ -475,7 +478,11 @@ ui.body.on('submit', '#credential-form', async e => {
         return;
     }
 
-    await repository.updateCredential(credential);
+    if (isNew) {
+        await repository.createCredential(credential);
+    } else {
+        await repository.updateCredential(credential);
+    }
 
     const updatedCredentials = await repository.loadCredentialSummaryList();
 

Vault/Controllers/CredentialsController.cs
@@ -1,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Dapper;
+using Vault.Models;
+using Vault.Support;
+
+namespace Vault.Controllers
+{
+    public class CredentialsController : Controller
+    {
+        private SqlExecutor _db;
+
+        public CredentialsController(IConnectionFactory cf) => _db = new SqlExecutor(cf);
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Credential model) =>
+            await _db.ResultAsJson(conn => conn.ExecuteAsync(SqlStatements.Insert, model));
+
+        [HttpPost]
+        public async Task<ActionResult> Read(string id) =>
+            await _db.ResultAsJson(conn => conn.QuerySingleOrDefaultAsync<Credential>(SqlStatements.SelectSingle, new { CredentialID = id }));
+
+        [HttpPost]
+        public async Task<ActionResult> ReadAll(string userId) =>
+            await _db.ResultAsJson(conn => conn.QueryAsync<Credential>(SqlStatements.Select, new { UserID = userId }));
+
+        [HttpPost]
+        public async Task<ActionResult> ReadSummaries(string userId) =>
+            await _db.ResultAsJson(conn => conn.QueryAsync<CredentialSummary>(SqlStatements.SelectSummary, new { UserID = userId }));
+
+        [HttpPost]
+        public async Task<ActionResult> Update(Credential model) =>
+            await _db.ResultAsJson(conn => conn.ExecuteAsync(SqlStatements.Update, model));
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateMultiple(IList<Credential> model) =>
+            await _db.ResultAsJson(async (conn, tran) => {
+                foreach (var credential in model)
+                {
+                    credential.CredentialID = Guid.NewGuid().ToString();
+                    await conn.ExecuteAsync(SqlStatements.Insert, credential, tran);
+                }
+                return Json(new { done = true });
+            });
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string credentialId, string userId) =>
+            await _db.ResultAsJson(conn => conn.ExecuteAsync(SqlStatements.Delete, new { UserID = userId, CredentialID = credentialId }));
+    }
+}

Vault/Models/Credential.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace Vault.Models
 {
@@ -23,7 +24,7 @@ namespace Vault.Models
 
         public Credential()
         {
-            CredentialID = "{{CredentialID}}";
+            CredentialID = Guid.NewGuid().ToString();
             UserID = "{{UserID}}";
             Description = "{{Description}}";
             Username = "{{Username}}";

Vault/Views/Shared/_CredentialForm.cshtml
@@ -86,5 +86,6 @@
     </div>
 
     @Html.HiddenFor(x => x.CredentialID)
+    @Html.Hidden("new", "{{new}}")
                 
 }

Vault/Content/Js/tests/FakeRepository.ts
@@ -101,6 +101,10 @@ export class FakeRepository implements IRepository {
         return this.credentials;
     }
 
+    public async createCredential(credential: ICredential) {
+        return new Promise<ICredential>(resolve => resolve());
+    }
+
     public async updateCredential(credential: ICredential) {
         return new Promise<ICredential>(resolve => resolve());
     }

Vault/Content/Js/types/IRepository.ts
@@ -7,6 +7,7 @@ export interface IRepository {
     loadCredentialSummaryList(): Promise<ICredential[]>;
     loadCredentials(): Promise<ICredential[]>;
 
+    createCredential(credential: ICredential): Promise<ICredential>;
     updateCredential(credential: ICredential): Promise<ICredential>;
     updatePassword(newPassword: string): Promise<void>;
     updateMultiple(credentials: ICredential[]): Promise<void>;

Vault/Content/Js/types/Repository.ts
@@ -47,28 +47,35 @@ export class Repository implements IRepository {
     }
 
     public async loadCredential(credentialId: string) {
-        const encryptedCredential = await this.post<ICredential>('Main/LoadCredential', { id: credentialId });
+        const encryptedCredential = await this.post<ICredential>('Credentials/Read', { id: credentialId });
         return decryptCredential(encryptedCredential, this.masterKey, this.encryptionExcludes);
     }
 
     public async loadCredentialSummaryList() {
         if (!this.cache.length) {
-            const encryptedCredentials = await this.post<ICredential[]>('Main/GetCredentialSummaryList', { userId: this.userID });
+            const encryptedCredentials = await this.post<ICredential[]>('Credentials/ReadSummaries', { userId: this.userID });
             this.cache = decryptCredentials(encryptedCredentials, this.masterKey, this.encryptionExcludes);
         }
         return this.cache;
     }
 
     public async loadCredentials() {
-        const encryptedCredentials = await this.post<ICredential[]>('Main/GetCredentials', { userId: this.userID });
+        const encryptedCredentials = await this.post<ICredential[]>('Credentials/ReadAll', { userId: this.userID });
         return decryptCredentials(encryptedCredentials, this.masterKey, this.encryptionExcludes);
+    }
+
+    public async createCredential(credential: ICredential) {
+        this.cache.length = 0;
+        credential.UserID = this.userID;
+        const encryptedCredential = encryptCredential(credential, this.masterKey, this.encryptionExcludes);
+        return this.post<ICredential>('Credentials/Create', encryptedCredential);
     }
 
     public async updateCredential(credential: ICredential) {
         this.cache.length = 0;
         credential.UserID = this.userID;
         const encryptedCredential = encryptCredential(credential, this.masterKey, this.encryptionExcludes);
-        return this.post<ICredential>('Main/UpdateCredential', encryptedCredential);
+        return this.post<ICredential>('Credentials/Update', encryptedCredential);
     }
 
     public async updatePassword(newPassword: string) {
@@ -82,21 +89,22 @@ export class Repository implements IRepository {
         this.password = newPassword;
         this.masterKey = generateMasterKey(newPassword);
 
-        await this.updateMultiple(credentials);
+        const reEncryptedCredentials = encryptCredentials(credentials, this.masterKey, this.encryptionExcludes);
 
-        const data = {
-            userid: this.userID,
-            oldHash: oldPasswordHash,
-            newHash: newPasswordHash
+        const model = {
+            UpdatedCredentials: reEncryptedCredentials,
+            UserID: this.userID,
+            OldPasswordHash: oldPasswordHash,
+            NewPasswordHash: newPasswordHash
         };
 
-        await this.post<void>('Main/UpdatePassword', data);
+        await this.post<void>('Main/UpdatePassword', model);
     }
 
     public async updateMultiple(credentials: ICredential[]) {
         this.cache.length = 0;
         const encryptedCredentials = encryptCredentials(credentials, this.masterKey, this.encryptionExcludes);
-        return this.post<void>('Main/UpdateMultipleCredentials', JSON.stringify(encryptedCredentials), this.jsonContentType);
+        return this.post<void>('Credentials/UpdateMultiple', JSON.stringify(encryptedCredentials), this.jsonContentType);
     }
 
     public async deleteCredential(credentialId: string) {
@@ -105,7 +113,7 @@ export class Repository implements IRepository {
             userId: this.userID,
             credentialId: credentialId
         };
-        return this.post<void>('Main/DeleteCredential', data);
+        return this.post<void>('Credentials/Delete', data);
     }
 
     private post<T>(url: string, data: any, contentType: string = null) {

commit 3c59a863ff7d91331211ca09e4840579c80c4adb

Fix persistence bug introduced in b3f0185a, simplify

Vault/Content/Js/main.ts
@@ -249,11 +249,9 @@ async function editCredential(credentialId: string) {
 
     ui.spinner.hide();
 
-    const templateModel = Object.assign({ new: false }, credential);
-
     showModal({
         title: 'Edit Credential',
-        content: templates.credentialForm(templateModel),
+        content: templates.credentialForm(credential),
         showAccept: true,
         acceptText: 'Save',
         onaccept: (): void => {
@@ -426,7 +424,7 @@ ui.newButton.on('click', e => {
     e.preventDefault();
     showModal({
         title: 'Add Credential',
-        content: templates.credentialForm({ new: true }),
+        content: templates.credentialForm({}),
         showAccept: true,
         acceptText: 'Save',
         onaccept: (): void => {
@@ -490,8 +488,6 @@ ui.body.on('submit', '#credential-form', async e => {
     $('.validation-message').remove();
     form.find('div.has-error').removeClass('has-error');
 
-    const isNew = form.find('[name=new]').val() === 'true';
-
     const credential = getCredentialFromUI(form);
 
     const errors = validateCredential(credential);
@@ -508,7 +504,7 @@ ui.body.on('submit', '#credential-form', async e => {
 
     ui.spinner.show();
 
-    if (isNew) {
+    if (!credential.CredentialID) {
         await repository.createCredential(credential);
     } else {
         await repository.updateCredential(credential);

Vault/Controllers/CredentialsController.cs
@@ -17,7 +17,7 @@ namespace Vault.Controllers
 
         [HttpPost]
         public async Task<ActionResult> Create(Credential model) =>
-            await _db.ResultAsJson(conn => conn.ExecuteAsync(SqlStatements.Insert, model));
+            await _db.ResultAsJson(conn => conn.ExecuteAsync(SqlStatements.Insert, model.WithNewID()));
 
         [HttpPost]
         public async Task<ActionResult> Read(string id) =>

Vault/Models/Credential.cs
@@ -25,7 +25,7 @@ namespace Vault.Models
 
         public Credential()
         {
-            CredentialID = Guid.NewGuid().ToString();
+            CredentialID = "{{CredentialID}}";
             UserID = "{{UserID}}";
             Description = "{{Description}}";
             Username = "{{Username}}";
@@ -37,6 +37,13 @@ namespace Vault.Models
             UserDefined2 = "{{UserDefined2}}";
             Notes = "{{Notes}}";
             PwdOptions = "{{PwdOptions}}";
+        }
+
+        public Credential WithNewID()
+        {
+            var credentialWithGuid = (Credential)MemberwiseClone();
+            credentialWithGuid.CredentialID = Guid.NewGuid().ToString();
+            return credentialWithGuid;
         }
     }
 }

Vault/Views/Shared/_CredentialForm.cshtml
@@ -84,6 +84,5 @@
     </div>
 
     @Html.HiddenFor(x => x.CredentialID)
-    @Html.Hidden("new", "{{new}}")
 
 }