8edda9db29b1e81983c293dec7dbd530b4871365
Mark Bell <me@markb.co.uk>
2019-04-07 08:20:41 +0100

    Quotes are now consistent in webpack.config.js

--- Vault/webpack.config.js
+++ Vault/webpack.config.js
@@ -1,19 +1,19 @@
-const ProvidePlugin = require("webpack").ProvidePlugin;
+const ProvidePlugin = require('webpack').ProvidePlugin;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     // Specify the output file name and location
     output: {
-        filename: "[name].js",
-        path: __dirname + "/wwwroot/js/dist",
-        library: "Vault"
+        filename: '[name].js',
+        path: __dirname + '/wwwroot/js/dist',
+        library: 'Vault'
     },
     entry: {
-        'main': "./wwwroot/js/main.ts"
+        'main': './wwwroot/js/main.ts'
     },
     // Enable sourcemaps for debugging webpack's output
-    devtool: "source-map",
+    devtool: 'source-map',
     plugins: [
         // Remove the output folder before build
         new CleanWebpackPlugin({
@@ -22,8 +22,8 @@ module.exports = {
         // This automatically adds aliases to the application scope for the specified packages
         // So packages which look for the 'jQuery' global alias still work within our app closure
         new ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery'
         }),
         // Copy the external dependencies into the output folder
         new CopyWebpackPlugin([
@@ -35,21 +35,21 @@ module.exports = {
     ],
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.json']
     },
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
+            { test: /\.tsx?$/, exclude: /node_modules/, loader: 'ts-loader' },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
         ]
     },
     // When importing a module whose path matches one of the following keys, just
     // assume a global variable named <value> exists and use that instead
     externals: {
-        jquery: "jQuery",
-        handlebars: "Handlebars",
-        "js-cookie": "Cookies"
+        jquery: 'jQuery',
+        handlebars: 'Handlebars',
+        'js-cookie': 'Cookies'
     }
 };
