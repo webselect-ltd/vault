51024e1d3f6ac78145a61057fe0ca24bf01ff428
Mark Bell <me@markb.co.uk>
2024-06-14 14:48:36 +0100

    Initial SQLite schema

--- db/sqlite_schema.sql
+++ db/sqlite_schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS Credentials (
     "UserDefined1Label" NVARCHAR NULL,
     "UserDefined2" NVARCHAR NULL,
     "UserDefined2Label" NVARCHAR NULL,
-    "PwdOptions" NVARCHAR NULL
+    "PwdOptions" NVARCHAR NULL,
+    FOREIGN KEY ("UserID") REFERENCES Users ("UserID")
 );
 
 CREATE TABLE IF NOT EXISTS Users (
@@ -19,3 +20,17 @@ CREATE TABLE IF NOT EXISTS Users (
     "Password" NVARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Tags (
+    "TagID" NVARCHAR(36) PRIMARY KEY NOT NULL,
+    "UserID" NVARCHAR(36) NOT NULL,
+    "Label" NVARCHAR(64) NOT NULL,
+    FOREIGN KEY ("UserID") REFERENCES Users ("UserID")
+);
+
+CREATE TABLE IF NOT EXISTS Tags_Credentials (
+    "TagID" NVARCHAR(36) NOT NULL,
+    "CredentialID" NVARCHAR(36) NOT NULL,
+    PRIMARY KEY ("TagID", "CredentialID"),
+    FOREIGN KEY ("TagID") REFERENCES Tags ("TagID"),
+    FOREIGN KEY ("CredentialID") REFERENCES Credentials ("CredentialID")
+);
